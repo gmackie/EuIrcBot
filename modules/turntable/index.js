b97cd1ec6b0091e31949b032bb66a18e258e05fa
Graham Mackie <mackieg@umich.edu>
2014-07-20 00:27:31 -0400

    got module kinda working

--- modules/turntable/index.js
+++ modules/turntable/index.js
@@ -1,31 +1,37 @@
 var http = require('http');
 var https = require('https');
+var querystring = require('querystring');
 var config;
 
 module.exports.commands = ["turntable", "tt", "playlist"];
 
 module.exports.init = function(bot) {
+  console.log("started turntable module");
 };
 
 module.exports.run = function(remainder, parts, reply, command, from, to, text, raw){
       switch(command)
       {
         case "tt":
-          return turntable(remainder, from, reply);
+          return turntable(remainder, from, to, reply);
         case "turntable":
-          return turntable(remainder, from, reply);
+          return turntable(remainder, from, to, reply);
         default:
           return reply("Error, command <" + command + "> not implemented!");
       }
 
 };
 
-function turntable(remainder, from, reply) {
+function turntable(remainder, from, to, reply) {
   var command, args;
   args = remainder.split(" ");
   reply("command: " + args[0]);
   switch(args[0])
   {
+    case "join":
+      return joinRoom(from, reply, to);
+    case "leave":
+      return leaveRoom(from, reply, to);
     case "add":
       return addSong(args[1], from, reply);
     case "skip":
@@ -37,20 +43,94 @@ function turntable(remainder, from, reply) {
   }
 }
 
-function addSong(song_id, from, reply) {
+function leaveRoom(from, reply, to) {
   var post_data = querystring.stringify({
-    'hash' : song_id,
-    'room' : '#interns'
+    'username' : from
   });
   var post_options = {
     host: 'turntable.dongs.in',
     port: '80',
+    path: '/api/rooms/' + 'interns' + '/leave',
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': post_data.length
     }
+  };
+  var post_req = http.request(post_options, function(res) { 
+    var ttdata = '';
+    res.on('data', function(chunk) {
+      ttdata += chunk;
+    });
+    var turntableData;
+    res.on('end', function() {
+      try {
+        turntableData = JSON.parse(ttdata.toString());
+      } catch(e) {
+        return reply("Error handling turntable response");
+      }
+      if (turntableData.error) 
+      {
+        return reply("Error: " + turntableData.error);
+      }
+      return reply("Added user " + turntableData.user + " to the room: interns!");
+    });
   });
+  post_req.write(post_data);
+  post_req.end();
+}
+
+function joinRoom(from, reply, to) {
+  var post_data = querystring.stringify({
+    'username' : from
+  });
+  var post_options = {
+    host: 'turntable.dongs.in',
+    port: '80',
+    path: '/api/rooms/' + 'interns' + '/join',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_data.length
+    }
+  };
+  var post_req = http.request(post_options, function(res) { 
+    var ttdata = '';
+    res.on('data', function(chunk) {
+      ttdata += chunk;
+    });
+    var turntableData;
+    res.on('end', function() {
+      try {
+        turntableData = JSON.parse(ttdata.toString());
+      } catch(e) {
+        return reply("Error handling turntable response");
+      }
+      if (turntableData.error) 
+      {
+        return reply("Error: " + turntableData.error);
+      }
+      return reply("Added user " + turntableData.user + " to the room: interns!");
+    });
+  });
+  post_req.write(post_data);
+  post_req.end();
+}
+
+function addSong(song_id, from, reply) {
+  var post_data = querystring.stringify({
+    'hash' : song_id
+  });
+  var post_options = {
+    host: 'turntable.dongs.in',
+    port: '80',
+    path: '/api/songs',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Length': post_data.length
+    }
+  };
   var post_req = http.request(post_options, function(res) { 
     var ttdata = '';
     res.on('data', function(chunk) {
@@ -86,18 +166,18 @@ function addSong(song_id, from, reply) {
 function skipSong(from, reply) {
   var post_data = querystring.stringify({
     'username' : from,
-    'room' : '#interns'
+    'room' : 'interns'
   });
   var post_options = {
     host: 'turntable.dongs.in',
     port: '80',
-    path: '/api/room/#interns/skip',
+    path: '/api/room/' + 'interns' + '/skip',
     method: 'POST',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': post_data.length
     }
-  });
+  };
   var post_req = http.request(post_options, function(res) { 
     var ttdata = '';
     res.on('data', function(chunk) {
@@ -122,7 +202,7 @@ function skipSong(from, reply) {
 function queueSong(song_id, from, reply) {
   var post_data = querystring.stringify({
     'hash' : song_id,
-    'room' : '#interns'
+    'room' : 'interns'
   });
   var post_options = {
     host: 'turntable.dongs.in',
@@ -133,7 +213,7 @@ function queueSong(song_id, from, reply) {
       'Content-Type': 'application/x-www-form-urlencoded',
       'Content-Length': post_data.length
     }
-  });
+  };
   var post_req = http.request(post_options, function(res) { 
     var ttdata = '';
     res.on('data', function(chunk) {
